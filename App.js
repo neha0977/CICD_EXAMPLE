f8c6e995fa3b45f8e69d8217529d575d2ae13c30
neha0977 <nehasingh3@virtualemployee.com>
2024-07-05 14:02:13 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>CICD</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
---
+++ ios/Pods/Target Support Files/Pods-CICDEX-CICDEXTests/Pods-CICDEX-CICDEXTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CICDEX_CICDEXTests : NSObject
+@end
+@implementation PodsDummy_Pods_CICDEX_CICDEXTests
+@end
